4d2919581dc56d47e96dfa744bb0a6ce58bfbc72
jejemaes <jerome.2611@gmail.com>
2024-12-06 23:23:27 +0100

    WIP restfull API basics

---
+++ src/core/api/controller.py
@@ -0,0 +1,8 @@
+from ninja_extra import ControllerBase
+
+#--------------------------------------------------------
+# Model Controller Base Classes
+#--------------------------------------------------------
+
+class BaseModelController(ControllerBase):
+    pass
